702aa64f724db46cf0809aaf87adcecde26aa7d6
Joshua Alvarado <joshalva3@gmail.com>
2022-11-04 14:27:05 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+let a = 1; 
+let b = 2; 
+// console.log(a);
+// console.log(b);
+// console.log(a + b);
+
+console.log('Hello World');
+
+const joshua = {
+    name: 'josh',
+    age: 29,
+    occupation: 'server'
+};
+
+const mike = {
+    name: 'idiot',
+    age: 28,
+    occupation: 'taco maker '
+};
+
+console.log(joshua, mike);
+
+const users = [joshua, mike];
+console.log(users);
+console.log(users[0].name);
+
