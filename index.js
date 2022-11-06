0550fd78f3312a3be424904e0222750cb8f77f48
Joshua Alvarado <joshalva3@gmail.com>
2022-11-05 21:41:49 -0500

    control flow and boolean logic notes

--- index.js
+++ index.js
@@ -1,37 +1,55 @@
-let a = 1; 
-let b = 2; 
+// let a = 1; 
+// let b = 2; 
 // console.log(a);
 // console.log(b);
 // console.log(a + b);
 
-console.log('Hello World');
+// console.log('Hello World');
 
-const joshua = {
-    name: 'josh',
-    age: 29,
-    occupation: 'server'
-};
+// const joshua = {
+//     name: 'josh',
+//     age: 29,
+//     occupation: 'server'
+// };
 
-const mike = {
-    name: 'idiot',
-    age: 28,
-    occupation: 'taco maker '
-};
+// const mike = {
+//     name: 'idiot',
+//     age: 28,
+//     occupation: 'taco maker '
+// };
 
-console.log(joshua, mike);
+// console.log(joshua, mike);
 
-const users = [joshua, mike];
-console.log(users);
-console.log(users[0].name);
+// const users = [joshua, mike];
+// console.log(users);
+// console.log(users[0].name);
 
-const num1 = 1;
-const num2 = 2; 
-const result = num1 + num2;
+// const num1 = 1;
+// const num2 = 2; 
+// const result = num1 + num2;
 
-console.log(result);
+// console.log(result);
 
-const firstName = `joshua`;
-const lastName = `alvarado`;
-const fullName = firstName + ` ` + lastName;
+// const firstName = `joshua`;
+// const lastName = `alvarado`;
+// const fullName = firstName + ` ` + lastName;
 // console.log(fullName);
 // console.log(firstName + lastName);
+
+console.log(` <---- ORDER OF OPERATIONS ----> `);
+console.log(1 + 2 + 3);
+
+// multiplication comes before addition or subtraction 
+// parenthesis is called the grouping operator 
+
+
+let resNum = 1 + 2; // 3
+console.log(resNum);
+resNum += 1; // 4
+console.log(resNum);
+resNum++; // increment result
+resNum-- // decrement result 
+// += works on strings the rest only work on numbers
+
+// The addition assignment operator (+=) adds the value of the right operand to a variable and assigns the result to the variable. 
+
