2923a4c55d3ef436d893cef718aa2da53a05026b
Joshua Alvarado <joshalva3@gmail.com>
2022-11-05 21:59:40 -0500

    more notes on control flow lecture

---
+++ control-flow.js
@@ -0,0 +1,36 @@
+console.log(`<--- control flow --->`)
+// controlling the flow of the execution of the program.
+// let isItSunny = false; 
+// console.log(isItSunny);
+// console.log(typeof isItSunny);
+
+// if(isItSunny) { //inside these brackets are referred too as a 'block'
+//     console.log(`let's go to the beach`);
+// } else {
+//     console.log(`let's stay inside and read a book`);
+// }
+
+// example: determine what we can make based on ingredients we have 
+
+// let weHaveEggs = false; 
+// let weHaveBacon = true; 
+
+// if(weHaveEggs) {
+//     console.log(`make an omelet`);
+// }
+
+// if(weHaveBacon) {
+//     console.log(`make some bacon`);
+// }
+
+let a = 4; 
+let b = 7;
+// a === b : is a equal to b? 
+// a !== b : is a NOT equal to b? 
+// a > b : is a > b
+// a >= b : is a greater than or equal to b? 
+// a < b : is a less than b? 
+// a <= b : is a less than or equal to b? 
+
+// console.log(a === 6); // false
+// console.log(a = 6) //this is NOT a comparison operator. It's an assignment
