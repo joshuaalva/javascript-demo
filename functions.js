51079815f4774e2ed3af6c15483da28a4b3adeba
Joshua Alvarado <joshalva3@gmail.com>
2022-11-06 15:04:59 -0600

    starting functions examples

---
+++ functions.js
@@ -0,0 +1,28 @@
+console.log(`dese are functions`);
+
+let python = `Fira`;
+let pug = `Cody`;
+let bunny = `Eeyore`;
+
+// we may not want to write console.log and the same string three times 
+
+// console.log(python + ` The Magnificent`);
+// console.log(pug + ` The Magnificent`);
+// console.log(bunny + ` The Magnificent`);
+
+// function fn(param1, param2, param3){
+//     console.log(param1);
+//     console.log(param2);
+//     console.log(param3);
+// };
+
+// fn(1, 2, 3); // calling or invoking the function with arguements
+// fn(7, 8, 9);
+
+function example(name){
+    console.log(`${name}` + ` The Magnificent`);
+}
+
+example(python);
+example(pug);
+example(bunny);
