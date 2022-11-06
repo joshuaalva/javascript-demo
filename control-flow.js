5238979a1418d0a15f8e29526c9d276525c90bb9
Joshua Alvarado <joshalva3@gmail.com>
2022-11-06 14:38:07 -0600

    end of lecture, examples, operators, commented out code

--- control-flow.js
+++ control-flow.js
@@ -1,4 +1,4 @@
-console.log(`<--- control flow --->`)
+// console.log(`<--- control flow --->`)
 // controlling the flow of the execution of the program.
 // let isItSunny = false; 
 // console.log(isItSunny);
@@ -23,8 +23,12 @@ console.log(`<--- control flow --->`)
 //     console.log(`make some bacon`);
 // }
 
-let a = 4; 
-let b = 7;
+// let a = 4; 
+// let b = 7;
+
+// <---- COMPARISON OPERATORS ---->
+// do not use double equals 
+
 // a === b : is a equal to b? 
 // a !== b : is a NOT equal to b? 
 // a > b : is a > b
@@ -34,3 +38,83 @@ let b = 7;
 
 // console.log(a === 6); // false
 // console.log(a = 6) //this is NOT a comparison operator. It's an assignment
+
+// console.log(a = 6) // this is not comparison this is assignment 
+
+// let temp = 52; 
+// let jacket; 
+
+// if (temp > 50) {
+//     jacket = `no jacket`;
+// } else {
+//     jacket = `heavy jacket`
+// }
+
+// if (temp > 50 ) {
+//     jacket = `no jacket`;
+// } else if (temp > 50) {
+//     jacket = `light jacket`;
+// } else {
+//     jacket = `heavy jacket`;
+// }
+
+// else if preferred to nesting else
+
+// console.log(jacket);
+
+// ( <--- TRUTHY VS FALSEY ---> )
+// 8 falsey values really just 6 
+// false, 0, '', null, undefined, NaN, -0, 0n, */
+// everything else is true
+ 
+// let a = 6;
+
+// if (a) {
+//     console.log(`a is truthy`);
+// } else {
+//     console.log(`its zero`);
+// }
+
+// const name = `zer0`;
+
+// switch(name) {
+//     case `joshua`:
+//         console.log(`hello joshua`);
+//         break;
+//     case `alice`: 
+//         console.log(`hello alice`);
+//         break;
+//     default: 
+//         console.log(`idk your name`);
+// }
+
+// ( <--- LOGICAL OPERATORS ---> )
+// 3 main operators 
+// and = &&
+// or = ||
+// not = !
+
+// console.log(`and operator!!!!!`);
+
+// let pretzelsInStock = true; 
+// let amountOfChange = .60;
+
+// if (pretzelsInStock && amountOfChange >= 1.50) {
+//     console.log(`buy them thangs`);
+// } else {
+//     console.log(`dont buy them thangz`);
+// }
+
+console.log(`or operator!!!!!!!`)
+
+let rainy = false; 
+let excitingMovieOut = false; 
+let moneyIHave = 3.00;
+
+if (rainy || excitingMovieOut) {
+    console.log(`go to the movies`);
+} else{
+    console.log(`go to the park`);
+};
+
+ // you can put conditions into variables
